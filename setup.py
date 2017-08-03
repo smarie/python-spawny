5453c8dbfffa416c46630bcfc42fd3e396c2a8f8
Sylvain Marie <sylvain.marie@schneider-electric.com>
2017-08-03 14:36:16 +0200

    * coverage in pytest * travis: codecov integration

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ here = path.abspath(path.dirname(__file__))
 INSTALL_REQUIRES = []
 DEPENDENCY_LINKS = []
 SETUP_REQUIRES = ['pytest-runner','setuptools_scm', 'pypandoc', 'pandoc']
-TESTS_REQUIRE = ['pytest', 'pytest-logging']
+TESTS_REQUIRE = ['pytest', 'pytest-logging', 'pytest-cov']
 EXTRAS_REQUIRE = {}
 # simple check
 try:
