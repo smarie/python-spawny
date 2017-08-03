d2fbd1fe3c856577d12d1fe6843db59551ff6b36
Sylvain Marie <sylvain.marie@schneider-electric.com>
2017-08-03 15:58:18 +0200

    * added documentation badge * added missing requirement for mkdocs, and moved the doc requirements in a separate folder

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ here = path.abspath(path.dirname(__file__))
 # *************** Dependencies *********
 INSTALL_REQUIRES = []
 DEPENDENCY_LINKS = []
-SETUP_REQUIRES = ['pytest-runner','setuptools_scm', 'pypandoc', 'pandoc', 'mkdocs']
+SETUP_REQUIRES = ['pytest-runner','setuptools_scm', 'pypandoc', 'pandoc']
 TESTS_REQUIRE = ['pytest', 'pytest-logging', 'pytest-cov', 'psutil']
 EXTRAS_REQUIRE = {}
 # simple check
